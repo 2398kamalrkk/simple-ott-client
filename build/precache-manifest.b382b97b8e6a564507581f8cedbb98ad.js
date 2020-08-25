0f7529f6d5b10be80781f23468a6082599017647
Kamal Kannan <kamal@Kamals-MacBook-Air.local>
2020-08-25 21:13:11 +0530

    build folder

---
+++ build/precache-manifest.b382b97b8e6a564507581f8cedbb98ad.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9fdcb8c8e787e22b04109b0f14554b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c48c59d1e2b5a1363bb",
+    "url": "/static/css/2.b1d03f0c.chunk.css"
+  },
+  {
+    "revision": "07a33c4ba520a265bda5",
+    "url": "/static/css/main.34a024ff.chunk.css"
+  },
+  {
+    "revision": "6c48c59d1e2b5a1363bb",
+    "url": "/static/js/2.427466cc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.427466cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07a33c4ba520a265bda5",
+    "url": "/static/js/main.020be7d8.chunk.js"
+  },
+  {
+    "revision": "f05a52d2835a9263ed21",
+    "url": "/static/js/runtime-main.07807cc7.js"
+  },
+  {
+    "revision": "c3f84bd9d0ca95451c3fc4fda163801b",
+    "url": "/static/media/back-carousel.c3f84bd9.svg"
+  },
+  {
+    "revision": "475ae9929d6764a89fcc6a769ed62510",
+    "url": "/static/media/contract.475ae992.svg"
+  },
+  {
+    "revision": "2fc265a4997786088731dc39234e86a8",
+    "url": "/static/media/cross.2fc265a4.svg"
+  },
+  {
+    "revision": "35aa98a80be564ff006151866ef53387",
+    "url": "/static/media/expand.35aa98a8.svg"
+  },
+  {
+    "revision": "316769dba87c319d240c261155a3fa05",
+    "url": "/static/media/forward.316769db.svg"
+  },
+  {
+    "revision": "2c93416a9c3affa873b64b40ce5044c7",
+    "url": "/static/media/front-carousel.2c93416a.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7ea866b63fed2a54bf5664f4b41d083f",
+    "url": "/static/media/pause.7ea866b6.svg"
+  },
+  {
+    "revision": "da63417c140de3f0d1bbe6ad16fff831",
+    "url": "/static/media/play.da63417c.svg"
+  },
+  {
+    "revision": "6bc40ea0518861ce9b447dd76adc02c5",
+    "url": "/static/media/previous.6bc40ea0.svg"
+  }
+]);
